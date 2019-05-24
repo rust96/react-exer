8eed612f1744f0bb5028254245449690512f6364
rust96 <rust.96@bk.ru>
2019-05-24 08:29:28 +0300

    drag n drop ui almost ready

--- src/components/list-row/list-row.js
+++ src/components/list-row/list-row.js
@@ -3,17 +3,29 @@ import TaskCreator from '../task-creator';
 import './list-row.scss';
 
 
+const LastTask = (props) => {
+  return (
+      <span
+        className="task_last"
+        draggable
+        onDragEnter={props.onDragEnter}>
+      </span>
+  );
+}
+
 class ListRow extends Component {
 
   state = {
     margin: 0,
     marginMode: false,
-    editListIdx: null
+    editListIdx: null,
+    lastTask: false
   }
 
   render () {
    let marginT = 0;
    let marginB = 8;
+   let lastTaskClass = '';
    const { board: { cells, elements, id }, addTask } = this.props;
 
     const renderLists = ({ title, elements }, idx, boardId, fn) => {
@@ -23,13 +35,14 @@ class ListRow extends Component {
             <div className="tasks">
               <p className="list-title">{ title }</p>
               { renderTasks(elements, idx) }
+              <LastTask
+                onDragEnter={() => { this.setState({ lastTask: true }); console.log(this.state.lastTask)}} />
             </div>
             <TaskCreator
               boardId={boardId}
               listIdx={idx}
               addTask={fn} />
           </div>
-
         </div>
       );
     };
@@ -42,8 +55,12 @@ class ListRow extends Component {
 
 
       const dragEnter = (e, idx, listIdx) => {
-        this.setState({ margin: idx });
+        this.setState({
+          margin: idx,
+          lastTask: false
+        });
         this.setState({ editListIdx: listIdx })
+        console.log(idx)
 
         e.preventDefault();
         e.stopPropagation();
@@ -56,28 +73,34 @@ class ListRow extends Component {
       }
 
       return tasks.map((task, idx) => {
-        if (this.state.margin === idx && this.state.marginMode === true && listIdx === this.state.editListIdx) {
-          if (idx === tasks.length - 1) {
-            marginB = 50;
-            marginT = 50;
-          } else {
+        if (this.state.margin === idx && this.state.marginMode === true && listIdx === this.state.editListIdx && !this.state.lastTask) {
             marginT = 60;
             marginB = 8;
-          }
+        } else if (this.state.lastTask && listIdx === this.state.editListIdx && idx === tasks.length - 1) {
+          marginT = 0;
+          marginB = 50;
+        } else if (!this.state.marginMode) {
+          marginT = 0;
+          marginB = 8;
         } else {
           marginT = 0;
           marginB = 8;
         }
+
         return (
-          <div key={idx} style={{marginTop:marginT + 'px', marginBottom:marginB + 'px'}}
-            className="task"
-            draggable
-            onDragStart={(e) => dragStart(e, idx)}
-            onDragEnter={(e) => dragEnter(e, idx, listIdx)}
-            onDragOver={(e) => dragOver(e)}
-            onDrop={(e) => this.setState({ marginMode: false })}>
-              <p className="task__item">{ task }</p>
-          </div>
+          <React.Fragment key={idx}>
+            <div
+              style={{marginTop:marginT + 'px', marginBottom:marginB + 'px'}}
+              className="task"
+              draggable
+              onDragStart={(e) => dragStart(e, idx)}
+              onDragEnter={(e) => dragEnter(e, idx, listIdx)}
+              onDragOver={(e) => dragOver(e)}
+              onDragEnd={() => this.setState({ marginMode: false }) }
+              onDrop={(e) => this.setState({ marginMode: false }) }>
+                <p className="task__item">{ task }</p>
+            </div>
+          </React.Fragment>
         )
       })
     };
