ca2ce58de02a96ca42013e2a5813be478d5de83b
rust96 <rust.96@bk.ru>
2019-05-25 18:02:38 +0300

    ready for prod

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -99,26 +99,27 @@ const createTask = (state, action) => {
 }
 
 const dragTask = (state, action) => {
+  const { rmList, rmTask, addList, addTask, boardId } = action;
 
-  if (action.rmList === action.addList && action.rmTask === action.addTask) {
+  if (rmList === addList && rmTask === addTask) {
     return state;
   }
 
-  const removedTask = state.boards[action.boardId].cells[action.rmList].elements[action.rmTask];
-  const choosenTasksArr = state.boards[action.boardId].cells[action.addList].elements;
-  const choosenTasksCell = state.boards[action.boardId].cells[action.addList];
+  const removedTask = state.boards[boardId].cells[rmList].elements[rmTask];
+  const choosenTasksArr = state.boards[boardId].cells[addList].elements;
+  const choosenTasksCell = state.boards[boardId].cells[addList];
 
 
   let newAddTasks = [];
-  if (action.addTask === 0) {
+  if (addTask === 0) {
     newAddTasks = [removedTask, ...choosenTasksArr];
-  } else if (action.addTask === choosenTasksArr.length) {
+  } else if (addTask === choosenTasksArr.length) {
     newAddTasks = [...choosenTasksArr, removedTask];
   } else {
     newAddTasks = [
-      ...choosenTasksArr.slice(0, action.addTask),
+      ...choosenTasksArr.slice(0, addTask),
       removedTask,
-      ...choosenTasksArr.slice(action.addTask)
+      ...choosenTasksArr.slice(addTask)
     ]
   }
 
@@ -128,20 +129,20 @@ const dragTask = (state, action) => {
   }
 
   let newListsArray = [
-      ...state.boards[action.boardId].cells.slice(0, action.addList),
+      ...state.boards[boardId].cells.slice(0, addList),
       newCellAdd,
-      ...state.boards[action.boardId].cells.slice(action.addList + 1)
+      ...state.boards[boardId].cells.slice(addList + 1)
     ];
 
   const newBoard = {
-    ...state.boards[action.boardId],
+    ...state.boards[boardId],
     cells: newListsArray
   };
 
   const newBoards = [
-    ...state.boards.slice(0, action.boardId),
+    ...state.boards.slice(0, boardId),
     newBoard,
-    ...state.boards.slice(action.boardId + 1)
+    ...state.boards.slice(boardId + 1)
   ]
 
   const newState = {
@@ -149,28 +150,28 @@ const dragTask = (state, action) => {
     boards: newBoards
   }
 
-  const removingCell = newState.boards[action.boardId].cells[action.rmList];
+  const removingCell = newState.boards[boardId].cells[rmList];
 
   let newRmTasks = [];
-  if (action.rmList === action.addList && action.rmTask < action.addTask) {
+  if (rmList === addList && rmTask < addTask) {
     newRmTasks = [
-      ...removingCell.elements.slice(0, action.rmTask),
-      ...removingCell.elements.slice(action.rmTask + 1)
+      ...removingCell.elements.slice(0, rmTask),
+      ...removingCell.elements.slice(rmTask + 1)
     ]
-  } else if (action.rmList === action.addList && action.rmTask > action.addTask) {
+  } else if (rmList === addList && rmTask > addTask) {
     newRmTasks = [
-      ...removingCell.elements.slice(0, action.rmTask + 1),
-      ...removingCell.elements.slice(action.rmTask + 2)
+      ...removingCell.elements.slice(0, rmTask + 1),
+      ...removingCell.elements.slice(rmTask + 2)
     ]
 
-  } else if (action.rmTask === 0) {
-    newRmTasks = [...removingCell.elements.slice(action.rmTask + 1)];
-  } else if (action.rmTask === removingCell.elements.length - 1) {
-    newRmTasks = [...removingCell.elements.slice(0, action.rmTask)];
+  } else if (rmTask === 0) {
+    newRmTasks = [...removingCell.elements.slice(rmTask + 1)];
+  } else if (rmTask === removingCell.elements.length - 1) {
+    newRmTasks = [...removingCell.elements.slice(0, rmTask)];
   } else {
     newRmTasks = [
-      ...removingCell.elements.slice(0, action.rmTask),
-      ...removingCell.elements.slice(action.rmTask + 1)
+      ...removingCell.elements.slice(0, rmTask),
+      ...removingCell.elements.slice(rmTask + 1)
     ]
   }
 
@@ -180,20 +181,20 @@ const dragTask = (state, action) => {
   }
 
   let newRmLists = [
-    ...newState.boards[action.boardId].cells.slice(0, action.rmList),
+    ...newState.boards[boardId].cells.slice(0, rmList),
     newRmCell,
-    ...newState.boards[action.boardId].cells.slice(action.rmList + 1)
+    ...newState.boards[boardId].cells.slice(rmList + 1)
   ];
 
   const newRmBoard = {
-    ...newState.boards[action.boardId],
+    ...newState.boards[boardId],
     cells: newRmLists
   };
 
   const newRmBoards = [
-    ...newState.boards.slice(0, action.boardId),
+    ...newState.boards.slice(0, boardId),
     newRmBoard,
-    ...newState.boards.slice(action.boardId + 1)
+    ...newState.boards.slice(boardId + 1)
   ]
 
   return {
