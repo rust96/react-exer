af6dd3d0beace66fed18b2501d1b70660c310b96
rust96 <rust.96@bk.ru>
2019-05-25 19:20:18 +0300

    Updates

---
+++ precache-manifest.da96204e6ba283ac5db951d48d46184b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7c8ed89af7773e709f3",
+    "url": "/react-exer/static/js/runtime~main.21a97f7f.js"
+  },
+  {
+    "revision": "6ca1d0583777ae70e6c0",
+    "url": "/react-exer/static/js/main.3157a6dd.chunk.js"
+  },
+  {
+    "revision": "2b6c89aadd6a06fb0e84",
+    "url": "/react-exer/static/js/2.fb4b35a1.chunk.js"
+  },
+  {
+    "revision": "6ca1d0583777ae70e6c0",
+    "url": "/react-exer/static/css/main.da6601ff.chunk.css"
+  },
+  {
+    "revision": "5ff5d8270c258861ec8509deeca37ceb",
+    "url": "/react-exer/index.html"
+  }
+];
