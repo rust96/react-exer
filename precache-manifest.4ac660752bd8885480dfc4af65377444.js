0dfb2c01563a7587a6396d21c49aa54658eb8701
rust96 <rust.96@bk.ru>
2019-05-25 19:47:08 +0300

    Updates

--- precache-manifest.4ac660752bd8885480dfc4af65377444.js
+++ precache-manifest.4ac660752bd8885480dfc4af65377444.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-exer/static/js/runtime~main.21a97f7f.js"
   },
   {
-    "revision": "6ca1d0583777ae70e6c0",
-    "url": "/react-exer/static/js/main.3157a6dd.chunk.js"
+    "revision": "0330b9c353b07638d984",
+    "url": "/react-exer/static/js/main.4903bec3.chunk.js"
   },
   {
     "revision": "2b6c89aadd6a06fb0e84",
     "url": "/react-exer/static/js/2.fb4b35a1.chunk.js"
   },
   {
-    "revision": "6ca1d0583777ae70e6c0",
-    "url": "/react-exer/static/css/main.da6601ff.chunk.css"
+    "revision": "0330b9c353b07638d984",
+    "url": "/react-exer/static/css/main.4024ff90.chunk.css"
   },
   {
-    "revision": "5ff5d8270c258861ec8509deeca37ceb",
+    "revision": "17e856aa942f69480e8bf8deab3594c4",
     "url": "/react-exer/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-exer/precache-manifest.da96204e6ba283ac5db951d48d46184b.js"
+  "/react-exer/precache-manifest.4ac660752bd8885480dfc4af65377444.js"
 );
 
 workbox.clientsClaim();
