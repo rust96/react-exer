80016ed362ae3f8bb5e1a2af0a11e9820785b5fc
rust96 <rust.96@bk.ru>
2019-05-26 16:28:40 +0300

    update gh-pages error due to the absence of browser history

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <Router basename="/react-exer">
       <App />
     </Router>
   </Provider>, document.getElementById('root'));
